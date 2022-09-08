5da29f1f4f25a9f95ff05b11680a9c260dd1c6a4
kalraPrince <kalraprince75@gmail.com>
2022-09-08 21:41:50 +0530

    footer is added

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,12 +1,12 @@
+import Footer from "./Components/Footer";
 import Slider from "./Pages/UserDashBoard";
 
 import UserProduct from "./Pages/UserProduct";
 function App() {
   return (
     <div>
-      <Slider></Slider>
-     
-        <UserProduct></UserProduct>
+      {/* <UserProduct></UserProduct> */}
+      <Footer></Footer>
    
     </div>
   );
